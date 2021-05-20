fde1c99be8a92521e79eefcf049407b4295f229f
diego-graziati <dgraziati@gmail.com>
2021-05-20 22:46:34 +0200

    Added significant changes to the project. New classes and the whole GUI have been created.

--- src/src/controllers/Controller.java
+++ src/src/controllers/Controller.java
@@ -1,10 +1,69 @@
 package src.controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import src.athlets.AthletsViews;
 
-public class Controller {
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+//In this class all the events triggered in the mainPage.fxml are coded
+public class Controller implements Initializable {
 
     @FXML
-    Label labelTest;
+    public Tab fileLoaderTab;
+    @FXML
+    public Label fileLoaderLab;
+    @FXML
+    public ToggleButton fileLoaderBut;
+    @FXML
+    public Tab athletesViewsTab;
+    @FXML
+    public AnchorPane ancPaneViews;
+    @FXML
+    public ScrollPane scrollPaneViews;
+    @FXML
+    public Accordion accViews;
+
+
+    public static Tab static_fileLoaderTab;
+    public static Label static_fileLoaderLab;
+    public static ToggleButton static_fileLoaderBut;
+    public static Tab static_athletesViewsTab;
+    public static AnchorPane static_ancPaneViews;
+    public static ScrollPane static_scrollPaneViews;
+    public static Accordion static_accViews;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        static_fileLoaderTab=fileLoaderTab;
+        static_fileLoaderLab=fileLoaderLab;
+        static_fileLoaderBut=fileLoaderBut;
+        static_athletesViewsTab=athletesViewsTab;
+        static_ancPaneViews=ancPaneViews;
+        static_scrollPaneViews=scrollPaneViews;
+        static_accViews=accViews;
+
+        accordionInitializer(static_accViews);
+        LinkedList<AthletsViews> atView = new LinkedList<>();
+
+        int i;
+
+        for(i=0;i<50;i++){
+            String s = new String(Integer.toString(i));
+            atView.add(new AthletsViews());
+
+            static_accViews.getPanes().add(atView.get(i).TabCreator(s,"Sono figo e me ne vanto!"));
+        }
+    }
+
+    public void accordionInitializer(Accordion acc){
+        acc.prefWidthProperty().bind(static_scrollPaneViews.widthProperty());
+    }
 }
--- src/src/lang/JsonAnalyzer.java
+++ src/src/lang/JsonAnalyzer.java
@@ -8,23 +8,27 @@ import java.util.Scanner;
 //Everything MUST follow strict convention rules, so DO NOT change anything if you don't even know what this file is about!
 public class JsonAnalyzer {
 
-    public int fileOpener(String fileName){
+    public boolean fileOpener(String fileName){
         try{
             File inFile = new File(fileName);
             Scanner reader = new Scanner(inFile);
-            return 1;
+            return true;
         }catch(FileNotFoundException e){
             e.printStackTrace();
-            return 0;
+            return false;
         }
     }
 
-    public int fileNameComposer(String lang){
+    public String fileNameComposer(String lang){
         String fullFileName=lang;
 
         fullFileName.concat(".json");
         System.out.print(fullFileName);
 
-        return fileOpener(fullFileName);
+        if(fileOpener(fullFileName)){
+            return fullFileName;
+        }else{
+            return "NULL";
+        }
     }
 }
